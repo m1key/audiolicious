404c03ee09297a04d0ca27f6fc55dd9efe3ceca2
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2011-07-23 12:26:05 +0100

    Testing retrieving album by UUID.

--- audiolicious-domain-objects/src/test/java/me/m1key/audiolicious/domain/entities/AlbumHibernateIT.java
+++ audiolicious-domain-objects/src/test/java/me/m1key/audiolicious/domain/entities/AlbumHibernateIT.java
@@ -19,6 +19,7 @@
 package me.m1key.audiolicious.domain.entities;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -50,6 +51,7 @@ public class AlbumHibernateIT {
 	private static final String ARTIST_2_ALBUM_1_NAME = "Halim";
 
 	private EntityManager entityManager;
+	private Album artist1Album1;
 
 	@Deployment
 	public static Archive<?> createTestArchive()
@@ -78,20 +80,40 @@ public class AlbumHibernateIT {
 		List<?> allArtists = selectArtists.getResultList();
 		assertEquals("There should be two artists.", 2, allArtists.size());
 
-		Query select = entityManager.createQuery("FROM Album");
-		List<?> allAlbums = select.getResultList();
-		assertEquals("There should be three albums.", 3, allAlbums.size());
+		assertEquals("There should be three albums.", 3, getAllAlbums().size());
 	}
 
 	@Test
 	public void whenAllArtistsRemovedThereShouldBeNoAlbums() {
 		createAlbums();
 		deleteAllArtists();
+		assertEquals("There should be no albums after all artists removed.", 0,
+				getAllAlbums().size());
+	}
+
+	@Test
+	public void shouldHaveCorrectNumberOfAlbumsPerArtist() {
+		createAlbums();
+
+		Query selectArtists = entityManager.createQuery("FROM Artist");
+		List<?> allArtists = selectArtists.getResultList();
+		assertEquals("There should be two artists.", 2, allArtists.size());
 
 		Query select = entityManager.createQuery("FROM Album");
 		List<?> allAlbums = select.getResultList();
-		assertEquals("There should be no albums after all artists removed.", 0,
-				allAlbums.size());
+		assertEquals("There should be three albums.", 3, allAlbums.size());
+	}
+
+	@Test
+	public void shouldReturnAlbumByUuid() {
+		createAlbums();
+
+		Album retrievedAlbum = getAlbumByUuid(artist1Album1.getUuid());
+		assertNotNull(String.format(
+				"There should be a non-null album with UUID [%s].",
+				artist1Album1.getUuid()), retrievedAlbum);
+		assertEquals("Retrieved album should have correct name.",
+				ARTIST_1_ALBUM_1_NAME, retrievedAlbum.getName());
 	}
 
 	@After
@@ -99,11 +121,29 @@ public class AlbumHibernateIT {
 		deleteAllArtists();
 	}
 
+	private Album getAlbumByUuid(String uuid) {
+		entityManager.getTransaction().begin();
+		Album album = (Album) entityManager
+				.createQuery("FROM Album WHERE uuid = :uuid")
+				.setParameter("uuid", uuid).getResultList().get(0);
+		entityManager.getTransaction().commit();
+		return album;
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<Album> getAllAlbums() {
+		Query select = entityManager.createQuery("FROM Album");
+		return select.getResultList();
+	}
+
 	private void createAlbums() {
+		assertEquals("There should be no albums before any are created.", 0,
+				getAllAlbums().size());
+
 		entityManager.getTransaction().begin();
 
 		Artist artist1 = new Artist(ARTIST_1_NAME);
-		Album artist1Album1 = new Album(ARTIST_1_ALBUM_1_NAME, artist1,
+		artist1Album1 = new Album(ARTIST_1_ALBUM_1_NAME, artist1,
 				new Rating(80));
 		entityManager.persist(artist1Album1);
 		Album artist1Album2 = new Album(ARTIST_1_ALBUM_2_NAME, artist1,
