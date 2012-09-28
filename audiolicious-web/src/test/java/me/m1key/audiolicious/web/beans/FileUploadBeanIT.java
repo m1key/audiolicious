5671b8f6f4f5134dfaa8b831a85562caf435b51d
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-09-28 21:53:19 +0100

    Upgraded RichFaces to 4.2.2.Final.

--- audiolicious-web/src/test/java/me/m1key/audiolicious/web/beans/FileUploadBeanIT.java
+++ audiolicious-web/src/test/java/me/m1key/audiolicious/web/beans/FileUploadBeanIT.java
@@ -24,6 +24,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIPanel;
@@ -119,6 +120,34 @@ public class FileUploadBeanIT {
 		@Override
 		public long getSize() {
 			return 0;
+		}
+
+		@Override
+		public void delete() throws IOException {
+		}
+
+		@Override
+		public String getHeader(String arg0) {
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaderNames() {
+			return null;
+		}
+
+		@Override
+		public Collection<String> getHeaders(String arg0) {
+			return null;
+		}
+
+		@Override
+		public String getParameterName() {
+			return null;
+		}
+
+		@Override
+		public void write(String arg0) throws IOException {
 		}
 
 	}
